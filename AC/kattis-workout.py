ac5084351ff154abe9f31d0e6c98d3015473378e
Reiden Ong <65756407+reidenong@users.noreply.github.com>
2023-03-31 23:12:28 +0800

    Update kattis-workout.py

--- AC/kattis-workout.py
+++ AC/kattis-workout.py
@@ -1,9 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define int long long int
+
+const int MAXN = 20000001;
 pair<int,int> jim[11];          // Array to track jim time
-bool ppl[11][5000001];    // Array to simulate time
-int delay[11] = {0};            // Array to track delays
+bool ppl[10][MAXN];    // Array to simulate time
+int delay[10] = {0};            // Array to track delays
+int delay_MOD[10];
 
 int main(){
     int x, y;
@@ -17,9 +21,11 @@ int main(){
     // Received other peoples input (u,r,t)
     for(int i = 0; i < 10; i++){
         cin >> u >> r >> t;
+
+        delay_MOD[i] = u+r;
         
         // Simulate for each machine
-        for(int j = t; j < 5000001; j++){
+        for(int j = t; j < MAXN; j++){
             if( (j - t )%(u + r) < u )ppl[i][j] = 1;
         }
         
@@ -37,17 +43,24 @@ int main(){
     while(1){
         
         // Gymming
-        if(ppl[curr_machine][curr_time] == 0 && waiting){
+        if(ppl[curr_machine][curr_time - delay[curr_machine]] == 0 && waiting){
             gymming = 1;
             waiting = 0;
         }
         if(gymming){
             time_left_mac--;
-            if(ppl[curr_machine][curr_time]){
-                for(int j = curr_time; j < 5000000; j++){
-                    ppl[curr_machine][j] = container;
+            if(ppl[curr_machine][curr_time - delay[curr_machine]]){
+                delay[curr_machine]++;
+                delay[curr_machine] %= delay_MOD[curr_machine];
+
+                /*
+                ppl[curr_machine][MAXN-1] = ppl[curr_machine][curr_time];
+                for(int j = curr_time+1; j < MAXN-1; j++){
                     container = ppl[curr_machine][j];
+                    ppl[curr_machine][j] = ppl[curr_machine][MAXN-1];
+                    ppl[curr_machine][MAXN-1] = container;
                 }
+                */
             }
         }
 
@@ -60,7 +73,7 @@ int main(){
             if(curr_machine == 10){
                 curr_machine = 0;
                 rounds++;
-                cout << rounds <<"round done" << curr_time << endl;
+                // cout << rounds <<"round done" << curr_time << endl;
             }
             time_left_mac = jim[curr_machine].first, time_left_rec = jim[curr_machine].second;
         }
@@ -77,9 +90,10 @@ int main(){
 
 
         if(rounds == 3)break;
+        
         //if(curr_time >= 50)break;
     }
-        
+
     cout << curr_time - jim[9].second;   // dont include recovery time
 
     return 0;
