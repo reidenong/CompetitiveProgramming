39e7ed2cf06d363cfdd378ef6c914e5281770c99
Reiden Ong <65756407+reidenong@users.noreply.github.com>
2023-05-16 17:03:22 +0800

    Create template.cpp

---
+++ LibraryCode_CPP/template.cpp
@@ -0,0 +1,13 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+int main(){
+
+    return 0;
+}
