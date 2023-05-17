5b2a7c466410efe1bdf619faf497185646ba427d
Reiden Ong <65756407+reidenong@users.noreply.github.com>
2023-05-17 17:52:05 +0800

    Create basic_ds_reference.java

---
+++ LibraryCode_JAVA/basic_ds_reference.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class App {
+    // Classic Global Array of Integers
+    static int[] arr = new int[101];
+  
+    // ArrayList, basically a vector
+    static ArrayList<Integer> arrLis = new ArrayList<Integer>();
+    arrLis.add(100);        // Append items to back 
+    arrLis.get(0);          // Reference by index
+    arrLis.set(0, 200);     // Modify an element, refer to index
+    arrLis.remove(0);       // Remove element by index
+    
+    Collections.sort(arrLis);           // Sorting
+    int arrLisSize = arrLis.size();     // Get size of arrayList
+    
+}
