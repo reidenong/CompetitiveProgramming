e693afbafcba4df24c740bb0167bafafb29e5ee0
Reiden Ong <65756407+reidenong@users.noreply.github.com>
2023-06-04 00:38:14 +0800

    Update and rename template.cpp to fast-template.cpp

--- LibraryCode_CPP/fast-template.cpp
+++ LibraryCode_CPP/fast-template.cpp
@@ -3,11 +3,10 @@
 #pragma GCC optimize("unroll-loops")
 #include <bits/stdc++.h>
 using namespace std;
-
-typedef long long ll;
 #define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
 int main(){
+    fast_cin();
 
     return 0;
 }
